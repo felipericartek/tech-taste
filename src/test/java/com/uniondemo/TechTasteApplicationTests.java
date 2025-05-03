cfd50aaea4aa3b2d4b0435a8dcc08c5761539758
felipericarte <felipericartem@gmail.com>
2025-05-03 20:40:07 -0300

    commit inicial

---
+++ src/test/java/com/uniondemo/TechTasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTasteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
